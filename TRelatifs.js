6257af71ed559eadabebfb14e6dd9c6f823e34a0
T Benab2143 <tarikbenabbou@gmail.com>
2016-08-18 03:23:42 +0100

    more precision

--- TRelatifs.js
+++ TRelatifs.js
@@ -5,17 +5,49 @@ if (NodeList.prototype.forEach === undefined) {
 }
 
 let terms = [{
-    time: 10,
-    divide: 1,
-    text: "%d secondes"
-}, {
-    time: 45,
-    divide: 1,
-    text: "moins d'une minutes"
-}, {
-    time: 3600,
+   time : 45,
+   divide: 60,
+   text: 'moins d\'une minute'
+},{
+    time: 90,
     divide: 60,
-    text: "%d minute"
+    text: 'environ une minute'
+},{
+    time: 45 * 60,
+    divide: 60,
+    text: "%d minutes"
+},{
+    time: 90 * 60,
+    divide: 60 * 60,
+    text: "environ une heure"
+},{
+    time: 24 * 60 * 60,
+    divide: 60 * 60,
+    text: "%d heures"
+},{
+    time: 42 * 60 * 60,
+    divide: 24 * 60 * 60,
+    text: "environ un jour"
+},{
+    time: 30 * 24 * 60 * 60,
+    divide: 24 * 60 * 60,
+    text: "%d jours"
+},{
+    time: 45 * 24 * 60 * 60,
+    divide: 24 * 60 * 60 * 30,
+    text: "environ un mois"
+},{
+    time: 365 * 24 * 60 * 60,
+    divide: 24 * 60 * 60 * 30,
+    text: "%d mois"
+},{
+    time: 365 * 1.5 * 24 * 60 * 60,
+    divide: 24 * 60 * 60 * 365,
+    text: "environ un an"
+},{
+    time: Infinity,
+    divide: 24 * 60 * 60 * 365,
+    text: "%d ans"
 }]
 
 document.querySelectorAll('[data-tr]').forEach(function(node) {
