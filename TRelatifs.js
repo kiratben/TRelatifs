2bf2a1f50a6426319bb9ee70e4bd7f1f2cda2456
T Benab2143 <tarikbenabbou@gmail.com>
2016-08-19 00:17:53 +0100

    Code optimization

--- TRelatifs.js
+++ TRelatifs.js
@@ -5,46 +5,46 @@ if (NodeList.prototype.forEach === undefined) {
 }
 
 let terms = [{
-   time : 45,
-   divide: 60,
-   text: 'moins d\'une minute'
-},{
+    time: 45,
+    divide: 60,
+    text: 'moins d\'une minute'
+}, {
     time: 90,
     divide: 60,
     text: 'environ une minute'
-},{
+}, {
     time: 45 * 60,
     divide: 60,
     text: "%d minutes"
-},{
+}, {
     time: 90 * 60,
     divide: 60 * 60,
     text: "environ une heure"
-},{
+}, {
     time: 24 * 60 * 60,
     divide: 60 * 60,
     text: "%d heures"
-},{
+}, {
     time: 42 * 60 * 60,
     divide: 24 * 60 * 60,
     text: "environ un jour"
-},{
+}, {
     time: 30 * 24 * 60 * 60,
     divide: 24 * 60 * 60,
     text: "%d jours"
-},{
+}, {
     time: 45 * 24 * 60 * 60,
     divide: 24 * 60 * 60 * 30,
     text: "environ un mois"
-},{
+}, {
     time: 365 * 24 * 60 * 60,
     divide: 24 * 60 * 60 * 30,
     text: "%d mois"
-},{
+}, {
     time: 365 * 1.5 * 24 * 60 * 60,
     divide: 24 * 60 * 60 * 365,
     text: "environ un an"
-},{
+}, {
     time: Infinity,
     divide: 24 * 60 * 60 * 365,
     text: "%d ans"
@@ -52,9 +52,8 @@ let terms = [{
 
 document.querySelectorAll('[data-tr]').forEach(function(node) {
     function setText() {
-        let seconds = Math.round((new Date()).getTime() / 1000 - parseInt(node.dataset.tr, 10));
-        let prefix = seconds > 0 ? 'il y a ' : 'Dans';
-        let wording = "";
+        let seconds = Math.floor((new Date()).getTime() / 1000 - parseInt(node.dataset.tr, 10));
+        let prefix = seconds > 0 ? 'Il y a ' : 'Dans ';
         let term = null;
         seconds = Math.abs(seconds);
 
@@ -64,17 +63,20 @@ document.querySelectorAll('[data-tr]').forEach(function(node) {
             }
         }
 
-        node.innerHTML = prefix + term.text.replace('%d',  Math.round(seconds / term.divide));
-
+        node.innerHTML = prefix + term.text.replace('%d', Math.round(seconds / term.divide));
+        let nextTick = seconds % term.divide
+        if (nextTick === 0) {
+            nextTick = term.divide;
+        }
         window.setTimeout(function() {
-         
-              if (window.requestAnimationFrame) {
-                window.requestAnimationFrame(setText);
-            } else {
-                setText();
-            }  
-                       
-        },1000)
+            if (node.parentNode) {
+                if (window.requestAnimationFrame) {
+                    window.requestAnimationFrame(setText);
+                } else {
+                    setText();
+                }
+            }
+        }, nextTick * 1000)
     }
     setText();
 })
---
+++ index.php
@@ -0,0 +1,11 @@
+<html>
+<head>
+
+</head>
+<body>
+A - <span data-tr = "<?= time() + 3600 ?>"></span><br/>
+B - <span data-tr = "<?= time()  ?>"></span><br/>
+C - <span data-tr = "<?= time() - 3600 ?>"></span>
+</body>    
+<script src="./TRelatifs.js"></script>
+</html>
